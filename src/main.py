434745f1aeac5983dedcfee22921fbcf0e2ba949
Tyler Allen <tyleruk2000@gmail.com>
2014-06-20 13:17:07 +0000

    Added src

---
+++ src/main.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python 
+from bottle import route,run,post,get,delete,static_file,request,response
+
+messages = []
+
+@get('/')
+def index():
+    return '''
+	<!DOCTYPE html>
+	<html>
+        <head>
+                <meta charset="UTF-8">
+                <title>pyQ</title>
+                <style>
+                        div
+                        {
+                        	margin: 0 auto;
+                        	width: 1280px;
+                        }
+                </style>
+        </head>
+        <body >
+                <div><h1>pyQ</h1></div>
+        </body>
+	</html>
+	'''
+
+@get('/message')
+def getMessage():
+	if len(messages) == 0:
+		return "\n"
+	else:
+		return messages.pop() + "\n"	
+
+@post('/message')
+def postMessage():
+	messages.append(request.query.message)
+	return "Added\n"
+
+run(host='localhost', port=8080, debug=True)
